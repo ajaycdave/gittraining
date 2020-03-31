f1af2a46e991045de884a7524399e6c78f9fa1c3
Ajay C Dave <ajayd@aum36.aum.com>
2020-03-31 09:58:18 +0530

    Add Twig Filter examples

---
+++ ajayd.twigtraining.36.aum/twig-filter.php
@@ -0,0 +1,17 @@
+<?php
+
+
+require_once __DIR__.'/bootstrap.php';
+ 
+$parameters = [
+'my_var' => 'Hello world !!!',
+'foo'=>'Test Twig Template file',
+'number'=>5
+];
+
+
+
+// Render our view
+echo $twig->render('twig-filter.html.twig', $parameters);
+
+?>
